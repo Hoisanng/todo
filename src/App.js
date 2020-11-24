9180b0fb3436c5cd97893054222d45b4dfa2950d
Hoisan Ng <hoisan.ng@gmail.com>
2020-11-24 16:39:22 +0100

    Kleuren en dagen toegevoegd aan kalender

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './App.css';
+import Dagen from './Dagen.json';
 
- 
+const dagen=Dagen
+console.log(dagen)
 function MyApp() {
   const [value, onChange] = useState(new Date());
  
@@ -11,6 +14,8 @@ function MyApp() {
       <Calendar
         onChange={onChange}
         value={value}
+        tileContent={({ date }) => {for (let i = 0; i < dagen.length; i++) if(`${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()}` === dagen[i].date ){return dagen[i].name;}}}
+        tileClassName={({ date }) => {for (let i = 0; i < dagen.length; i++) if(dagen[i].date === `${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()}`){return "color";}}}
       />
     </div>
   );
